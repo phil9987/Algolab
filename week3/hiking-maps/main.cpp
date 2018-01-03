bb9f7e8b245779dc4cc0d7b943c2a0f3a6ab9b08
Philip Junker <philipjunker@gmail.com>
2018-01-03 13:43:30 +0100

    avoid constructions, 60points

--- week3/hiking-maps/main.cpp
+++ week3/hiking-maps/main.cpp
@@ -9,49 +9,39 @@ typedef K::Triangle_2 T;
 
 using namespace std;
 
+inline bool contains(const vector<P>& t, const P& p) {
+    return !CGAL::right_turn(t[0],t[1],p) && !CGAL::right_turn(t[2],t[3],p) && !CGAL::right_turn(t[4],t[5],p);
+}
+
 void do_testcase() {
     int m, num_map_parts; cin >> m >> num_map_parts;
     int num_legs = m-1;
     vector<P> path(m);
     vector<T> maps(num_map_parts);
+    vector<vector<P> > triangles(num_map_parts);
     for(int i = 0; i < m; i++) {
         cin >> path.at(i);
     }
 
     for(int i = 0; i < num_map_parts; i++) {
-        vector<L> lines(3);
-        for(int j = 0; j < 3; j++) {
-            vector<P> ps(2);
-            for(int k = 0; k < 2; k++) cin >> ps.at(k);
-            lines.at(j) = L(ps.at(0), ps.at(1));
+        vector<P> points(6);
+        for(int j = 0; j < 6; j++) {
+            cin >> points.at(j);
         }
-        vector<P> vertices(3);
-        for(int j = 0; j < 3; j++) {
-            // calculate line intersections
-            auto o = CGAL::intersection(lines.at(j),lines.at((j+1)%3));
-            if (const P* op = boost::get<P>(&*o)){
-                vertices.at(j) = *op;
-            } else cerr << "ERROR: two lines do not intersect!" << endl;
+        // ensure correct order for orientation tests
+        for (std::size_t j = 0; j < 6; j+=2) {
+            if (CGAL::right_turn(points[j],points[j+1],points[(j+2)%6])) swap(points[j],points[j+1]);
         }
-        maps.at(i) = T(vertices.at(0), vertices.at(1), vertices.at(2));
+        triangles.at(i) = points;
     }
 
     vector<vector<bool>> contains_path(num_map_parts, vector<bool>(num_legs, false));
-    vector<vector<bool>> starting_at_map(num_map_parts, vector<bool>(num_legs, false));
-    vector<int> starting_at_map_stop_idx(num_map_parts, -1);
-    vector<int> cost(num_map_parts);
-    int min_cost = INT_MAX;
     for(int map_idx = 0; map_idx < num_map_parts; map_idx++) {
         for(int path_idx = 0; path_idx < num_legs; path_idx++) {
             T triangle = maps.at(map_idx);
-            if(!triangle.has_on_unbounded_side(path.at(path_idx)) && !triangle.has_on_unbounded_side(path.at(path_idx+1))) {
+            if(contains(triangles.at(map_idx), path.at(path_idx)) && contains(triangles.at(map_idx), path.at(path_idx +1))) {
                 contains_path.at(map_idx).at(path_idx) = true;
                 //clog << "map "  << map_idx << " contains path " << path_idx << endl;
-
-                for(int i = map_idx; i < num_map_parts; i++) {
-                    starting_at_map.at(i).at(path_idx) = true;
-                    //clog << "setting path " << path_idx << " for map " << i << endl;
-                }
             }
         }
     }
