6f1154b44cc9e44f54bd974d718b863c2a22cdcd
Philip Junker <philipjunker@gmail.com>
2018-01-07 21:13:53 +0100

    split & list method, tough to understand but medium difficult to implement

--- week5/light_at_the_museum/lm.cpp
+++ week5/light_at_the_museum/lm.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,10 +21,10 @@ bool is_zero(vector<int> j) {
     return true;
 }
 
-vector<int> change_switches(vector<int> start, bitset<30> switches) {
-    vector<int> end = start;
-    for(unsigned i = 0; i < num_switches; i++) {
-        if(switches.test(i)) {
+vector<int> change_switches(bitset<30> switches, int offset) {
+    vector<int> end(num_rooms, 0);
+    for(unsigned i = offset; i < num_switches; i++) {
+        if(switches.test(i-offset)) {
             // change this switch
             for(unsigned j = 0; j < end.size(); j++) {
                 end.at(j) += switch_circuit.at(i).at(j);
@@ -53,37 +54,52 @@ void do_testcase() {
         }
         switch_circuit.push_back(light_states);
     }
+
+    unsigned N1, N2;
+    N1 = num_switches / 2;
+    N2 = num_switches - N1;
+    vector<vector<int> > L2(1<<N2);
+    vector<pair<vector<int>, int> > L2_S(1<<N2);
+
+    // precompute 2^N/2 possibilities for N2
+    for(int i = 0; i < (1 << N2); i++) {
+        bitset<30> to_switch(i);
+        L2_S.at(i) = make_pair(change_switches(to_switch, N1), to_switch.count());
+    }
+
+    sort(L2_S.begin(), L2_S.end());
+    for(int i = 0; i < 1<<N2; i++) L2.at(i) = L2_S.at(i).first;
     size_t min_count = SIZE_MAX;
-    for(int i = 0; i < (1 << num_switches); i++) {
+
+    for(int i = 0; i < (1 << N1); i++) {
+        bitset<30> to_switch(i);
+        vector<int> l1 = change_switches(to_switch, 0);
+        vector<int> l2_wanted(num_rooms);
+        for(unsigned j = 0; j < num_rooms; j++) {
+            l2_wanted.at(j) = target_brightness.at(j) - initial_brightness.at(j) - l1.at(j);
+        }
+        auto it = lower_bound(L2.begin(), L2.end(), l2_wanted);
+        if(*it == l2_wanted) {
+            int idx = it - L2.begin();
+            min_count = min(min_count, L2_S.at(idx).second + to_switch.count());
+        }
+    }
+    
+
+
+
+    /*for(int i = 0; i < (1 << num_switches); i++) {
         bitset<30> to_switch(i);
         if(change_switches(initial_brightness, to_switch) == target_brightness) {
             min_count = min(min_count, to_switch.count());
         }
-    }
+    }*/
 
     if(min_count == SIZE_MAX) {
         cout << "impossible" << endl;
     } else {
         cout << min_count << endl;
     }
-
-
-    /*for(auto e: target_brightness) {
-        clog << e << ", ";
-    }
-    clog << endl;
-    auto a = switch_circuit.at(0);
-    clog << "a.size() = " << a.size() << endl;
-    for(auto e: switch_circuit.at(0)) {
-        clog << e << "; ";
-    }
-    clog << endl;
-    for(auto e: diff(target_brightness, switch_circuit.at(0))) {
-        clog << e << " ";
-    }
-    clog << endl;
-    clog << "." << endl;*/
-
 }
 
 int main() {
