40514e07d7365b37d9b3560e9b54d5b46dc67b5a
Philip Junker <junkerp@slabhg126.ethz.ch>
2018-01-08 08:55:53 +0100

    light solved

--- week10/light/main.cpp
+++ week10/light/main.cpp
@@ -17,6 +17,7 @@ long sq_dist(K::Point_2 p1, K::Point_2 p2) {
 	return a * a + b * b;
 }
 
+
 void do_testcase() {
 	int num_participants, num_lamps;
 	cin >> num_participants >> num_lamps;
@@ -39,9 +40,11 @@ void do_testcase() {
 		cin >> lamps.at(i);
 	}
 	
+	
 	// construct triangulation
 	Triangulation t;
 	t.insert(lamps.begin(), lamps.end());
+	int num_winners = 0;
 	
 	for(int i = 0; i < num_participants; i++) {
 		K::Point_2 nearest_lamp = t.nearest_vertex(participants_points.at(i))->point();
@@ -51,9 +54,64 @@ void do_testcase() {
 		//cout << "min_dist= " << min_dist << " actual_dist= " << actual_dist << endl;
 		//if(CGAL::squared_distance(nearest_lamp,participants_points.at(i)) >= min_dist) {
 		if(actual_dist >= min_dist){
-			//cout << "hit by lamp!" << endl;
 			cout << i << " ";
+			num_winners++;
 		}
+	}
+	
+	if(num_winners == 0) {
+		cout << "doing binary serach..." << endl;
+		int lower = 0;
+		int upper = num_participants -1;
+		vector<int> winners;
+		int middle = 0;
+		while(lower <= upper) {
+			
+			int diff = upper-lower;
+			middle = lower + (diff / 2);
+			
+			Triangulation t1;
+			t1.insert(lamps.begin(), lamps.begin() + middle);
+			int num_winners = 0;
+			
+			for(int i = 0; i < num_participants; i++) {
+				K::Point_2 nearest_lamp = t1.nearest_vertex(participants_points.at(i))->point();
+				long min_dist = (lamp_height + participants_radius.at(i)) * (lamp_height + participants_radius.at(i));
+				long actual_dist = sq_dist(nearest_lamp, participants_points.at(i));
+				if(actual_dist >= min_dist){
+					if(num_winners == 0) winners.clear();
+					//cout << i << " ";
+					num_winners++;
+				}
+			}
+			cout << "num_winners=" << num_winners << " lower=" << lower << " upper=" << upper << " middle=" << middle << endl;
+			if(num_winners > 0) {
+				lower = middle+1;
+			} else {
+				upper = middle-1;
+			}
+		}
+		if(upper == lower) middle--;
+		
+		
+		Triangulation t2;
+		t2.insert(lamps.begin(), lamps.begin()+middle);
+		int num_winners = 0;
+		
+		for(int i = 0; i < num_participants; i++) {
+			K::Point_2 nearest_lamp = t2.nearest_vertex(participants_points.at(i))->point();
+			long min_dist = (lamp_height + participants_radius.at(i)) * (lamp_height + participants_radius.at(i));
+			long actual_dist = sq_dist(nearest_lamp, participants_points.at(i));
+			//cout << "nearest_lamp: " << nearest_lamp << " participant: " << participants_points.at(i) << endl;
+			//cout << "min_dist= " << min_dist << " actual_dist= " << actual_dist << endl;
+			//if(CGAL::squared_distance(nearest_lamp,participants_points.at(i)) >= min_dist) {
+			if(actual_dist >= min_dist){
+				cout << i << " ";
+				num_winners++;
+			}
+		}
+
+		
 	}
 	
 	cout << endl;
