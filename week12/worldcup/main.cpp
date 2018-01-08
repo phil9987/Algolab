06d5237b6ba015c5be8db4d0ca680fa3ba1d195f
Philip Junker <junkerp@ethz.ch>
2018-01-08 13:27:03 +0100

    worldcup: added delunay to identify closest stadium/warehouse for every contour center

--- week12/worldcup/main.cpp
+++ week12/worldcup/main.cpp
@@ -3,13 +3,24 @@
 #include <CGAL/basic.h>
 #include <CGAL/QP_models.h>
 #include <CGAL/QP_functions.h>
-#include <CGAL/Gmpz.h>
+#include <CGAL/Gmpq.h>
+
+
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 P;
+typedef K::Circle_2 C;
+
+#include <CGAL/Delaunay_triangulation_2.h>
+typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
+typedef Triangulation::Vertex_handle Vertex_handle;
+
 // choose exact integral type
-typedef CGAL::Gmpz ET;
+typedef CGAL::Gmpq ET;
 typedef CGAL::Quotient<ET> SolT;
 
 
-typedef CGAL::Quadratic_program<long> Program;
+typedef CGAL::Quadratic_program<ET> Program;
 typedef CGAL::Quadratic_program_solution<ET> Solution;
 
 using namespace std;
@@ -29,17 +40,26 @@ void do_testcase(){
 	long num_contourlines;
 	cin >> num_warehouses >> num_stadiums >> num_contourlines;
 	vector<pair< long, long> > warehouses(num_warehouses);
+    vector<P> warehouse_location(num_warehouses);
+    vector<P> stadium_location(num_stadiums);
 	vector<pair< long, long> > stadiums(num_stadiums);
 	vector<vector<int> > rewards(num_warehouses);
+    vector<unsigned> warehouse_contour_count(num_warehouses, 0);
+    vector<unsigned> stadium_contour_count(num_stadiums, 0);
+    vector<vector<int> > contour_count(num_stadiums, vector<int>(num_warehouses, 0));
 	for(int i = 0; i < num_warehouses; i++) {
-		long x, y, supply, alcohol;
-		cin >> x >> y >> supply >> alcohol;
+        P loc;
+		long supply, alcohol;
+		cin >> loc >> supply >> alcohol;
 		warehouses.at(i) = make_pair(supply, alcohol);
+        warehouse_location.at(i) = loc;
 	}
 	for(int i = 0; i < num_stadiums; i++){
-		long x, y, demand, max_alcohol;
-		cin >> x >> y >> demand >> max_alcohol;
+		long demand, max_alcohol;
+        P loc;
+		cin >> loc >> demand >> max_alcohol;
 		stadiums.at(i) = make_pair(demand, max_alcohol);
+        stadium_location.at(i) = loc;
 	}
 	for(int i = 0; i < num_warehouses; i++) {
 		vector<int> tmp_rewards(num_stadiums);
@@ -48,10 +68,39 @@ void do_testcase(){
 		}
 		rewards.at(i) = tmp_rewards;
 	}
-	for(int i = 0; i < num_contourlines; i++) {
-		long x, y, radius;
-		cin >> x >> y >> radius;
+
+    Triangulation t;
+    t.insert(warehouse_location.begin(), warehouse_location.end());
+    t.insert(stadium_location.begin(), stadium_location.end());
+    
+	for(int cont = 0, num_filled_contour_lines = 0; cont < num_contourlines && num_filled_contour_lines <= 100; cont++) {
+		long radius;
+        P loc;
+		cin >> loc >> radius;
+        
+        long radius2 = radius*radius;
+        
+        P nearest = t.nearest_vertex(loc)->point();
+        if(CGAL::squared_distance(nearest, loc) < radius2) {
+            // there is something inside contourline!
+            C c(loc, radius2);
+            vector<int> w_contains_cont(num_warehouses, -1);
+            for(size_t s = 0; s < num_stadiums; s++) {
+                bool s_in_cont = c.has_on_bounded_side(stadium_location.at(s));
+                for(size_t w = 0; w < num_warehouses; w++) {
+                    if(w_contains_cont.at(w) == -1) {
+                        w_contains_cont.at(w) = c.has_on_bounded_side(warehouse_location.at(w));
+                    }
+                    bool w_in_cont = w_contains_cont.at(w);
+                    if((w_in_cont && !s_in_cont) || (!w_in_cont && s_in_cont)) {
+                        contour_count.at(s).at(w)++;
+                    }
+                    
+                }
+            }
+        }
 	}
+       
 	
 	// (1) sum of supplies for stadium = demand of stadium
 
@@ -84,23 +133,21 @@ void do_testcase(){
     // objective function: -sum of rewards (the solver minimizes)
     for(int s = 0; s < num_stadiums; s++) {
         for(int w = 0; w < num_warehouses; w++) {
-            lp.set_c(s*num_warehouses + w, -rewards.at(w).at(s));
+            lp.set_c(s*num_warehouses + w, -(ET(rewards.at(w).at(s))/1 - ET(contour_count.at(s).at(w))/100 ));
         }
     }
+    
+    //clog << "preparation done" << endl;
 
     // solve the program, using ET as the exact type
-    Solution s = CGAL::solve_linear_program(lp, ET());
+    Solution s = CGAL::solve_nonnegative_linear_program(lp, ET());
     assert (s.solves_linear_program(lp));
 
     // output exposure center and radius, if they exist
     if (s.is_optimal()) {
+        
+        cout << floor_to_double(-s.objective_value()) << endl;
 
-		if(s.objective_value() > 0) {
-			// result is negative
-			cout << -floor_to_double(s.objective_value()) << endl;
-		} else {
-			cout << floor_to_double(-s.objective_value()) << endl;
-		}
     } else
         std::cout << "RIOT!" << endl;
 	
