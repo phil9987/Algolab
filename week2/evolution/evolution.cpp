d1f1d3636ff6249686c77e86785a3e26c690d53c
Philip Junker <philipjunker@gmail.com>
2018-01-09 11:09:15 +0100

    preparation for dfs

--- week2/evolution/evolution.cpp
+++ week2/evolution/evolution.cpp
@@ -10,12 +10,12 @@
 
 using namespace std;
 
-void testcases() {
+void do_testcase() {
     int n, q; cin >> n >> q;
     map<string, int> species;
     vector<string> species_rev(n);
     vector<long> age(n);
-    map<int, int> ancestor;      //map id -> vector<ancestor_id, ancestor_age, expanded> 
+    vector<vector<int> > tree(n);       // the graph parent -> child
 
     for(int i = 0; i < n; i++) {
         string s; cin >> s;
@@ -27,42 +27,26 @@ void testcases() {
     for(int i = 0; i < n-1; i++) {
         string s, p;
         cin >> s >> p;
-        int s_idx, p_idx;
-        s_idx = species[s];
-        p_idx = species[p];
-        ancestor[s_idx] = p_idx;
+        tree.at(species[p]).push_back(species[s]);  // s is an immediate ofspring of p
     }
+    vector<vector<long> > queries(n);
 
     for(int i = 0; i < q; i++) {
         // handle the queries
         string s;
         long max_age;
         cin >> s >> max_age;
-        int s_idx = species[s];
-        int best_idx = s_idx;
-        auto it = ancestor.find(s_idx);
-        while(it != ancestor.end()) {
-            int a_idx = it->second;
-            //cout << "finding ancestor of " << s << " which is " << species_rev.at(a_idx) << " with age " << age.at(a_idx) << endl;
-            if(age.at(a_idx) <= max_age) {
-                best_idx = a_idx;
-                it = ancestor.find(best_idx);
-            } else {
-                it = ancestor.end();
-            }
-        }
-        cout << species_rev.at(best_idx) << " ";
+        queries.at(species[s]).push_back(max_age);
+
     }
     cout << endl;
-
-	
 }
 
 // Main function looping over the testcases
 int main() {
 	ios_base::sync_with_stdio(false); // if you use cin/cout. Do not mix cin/cout with scanf/printf calls!
 	int T;	cin >> T;
-	while(T--)	testcases();
+	while(T--)	do_testcase();
 	return 0;
 }
 
