12653e2758278ceee039f4a8522e1819fcb8ad98
Philip Junker <philipjunker@gmail.com>
2018-01-29 00:05:47 +0100

    init

---
+++ week12/radiation/main.cpp
@@ -0,0 +1,24 @@
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// Namespaces
+using namespace std;
+
+// Functions
+// =========
+// Function for an individual testcase
+void testcase() {
+
+}
+
+// Main function to loop over the testcases
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	int T;	cin >> T;
+	for (; T > 0; --T)	testcase();
+	return 0;
+}
