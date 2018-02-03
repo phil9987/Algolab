fd18e4179f945ff828216c97690238d94d9255b2
Philip Junker <philipjunker@gmail.com>
2018-02-03 22:51:16 +0100

    highschoolteams: split & list naive implementation

--- week11/highschoolteams/main.cpp
+++ week11/highschoolteams/main.cpp
@@ -4,12 +4,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "prettyprint.hpp"
-
+#include <set>
 using namespace std;
-int B = 22;
 
-vector<vector<int> > enumerate_all_possibilities(vector<vector<int> > s, int n, int N) {
+set<vector<int> > enumerate_all_possibilities(set<vector<int> > s, int n, int N) {
 	if(n < 0) return s;
 	else {
 		auto snew = s;
@@ -19,16 +17,16 @@ vector<vector<int> > enumerate_all_possibilities(vector<vector<int> > s, int n, 
 			b1.at(n) = 1;
 			vector<int> b2(N, 0);
 			b2.at(n) = 2;
-			snew.push_back(b0);
-			snew.push_back(b1);
-			snew.push_back(b2);
+			snew.insert(b0);
+			snew.insert(b1);
+			snew.insert(b2);
 		} else {
 			vector<int> b(N, 0);
 			for(auto seq: s) {
 				for(int i = 0; i < 3; i++) {
 					b = seq;
 					b.at(n) = i;
-					snew.push_back(b);
+					snew.insert(b);
 				}
 			}
 		}
@@ -45,14 +43,17 @@ void testcase() {
 		cin >> skills.at(i);
 	}
 
-	set<vector<int> > valid_constellations;
+	int n1 = n/2;
+	int n2 = n - n1;
 
-	for(auto b : enumerate_all_possibilities({}, n-1, n)) {
+	vector<pair<long, int> > possible_sums1;
+
+	for(auto b : enumerate_all_possibilities({}, n1-1, n1)) {
 		long sum_a = 0;
 		long sum_b = 0;
 		int k_count = 0;
 		bool valid = true;
-		for(int i = 0; i < n; i++) {
+		for(int i = 0; i < n1; i++) {
 			int el = b.at(i);
 			if(el == 0) {
 				sum_a += skills.at(i);
@@ -66,12 +67,52 @@ void testcase() {
 				}
 			}
 		}
-		if(valid && (sum_a - sum_b) == 0) {
-			valid_constellations.insert(b);
+		if(valid) {
+			possible_sums1.push_back(make_pair(sum_a - sum_b, k_count));
+		}
+	}
+
+	sort(possible_sums1.begin(), possible_sums1.end());		// sort ascending
+	vector<long> sums1;
+	vector<int> k1;
+	for(auto el: possible_sums1) {
+		sums1.push_back(el.first);
+		k1.push_back(el.second);
+	}
+
+	size_t num_possibilities = 0;
+	for(auto b : enumerate_all_possibilities({}, n2-1, n2)) {
+		long sum_a = 0;
+		long sum_b = 0;
+		int k_count = 0;
+		bool valid = true;
+		for(int i = 0; i < n2; i++) {
+			int el = b.at(i);
+			if(el == 0) {
+				sum_a += skills.at(n1+i);
+			} else if(el == 1) {
+				sum_b += skills.at(n1+i);
+			} else {
+				k_count++;
+				if(k_count > k) {
+					valid = false;
+					break;
+				}
+			}
+		}
+		if(valid) {
+			long q = -(sum_a - sum_b);
+			auto it = lower_bound(sums1.begin(), sums1.end(), q);
+
+			while(it != sums1.end() && *it == q && k1.at(it-sums1.begin()) + k_count <= k) {
+				num_possibilities++;
+				it++;
+			}
+
 		}
 
 	}
-	cout << valid_constellations.size() << endl;
+	cout << num_possibilities << endl;
 
 }
 
