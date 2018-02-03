d456128d21d12dd472a5e5f3f5d5b31a22632bd1
Philip Junker <philipjunker@gmail.com>
2018-02-03 23:36:38 +0100

    highschoolteams solved

--- week11/highschoolteams/main.cpp
+++ week11/highschoolteams/main.cpp
@@ -5,9 +5,10 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include "prettyprint.hpp"
 using namespace std;
 
-set<vector<int> > enumerate_all_possibilities(set<vector<int> > s, int n, int N) {
+vector<pair<vector<int>, int> > enumerate_all_possibilities(vector<pair<vector<int>, int> > s, int n, int N, int k) {
 	if(n < 0) return s;
 	else {
 		auto snew = s;
@@ -17,24 +18,34 @@ set<vector<int> > enumerate_all_possibilities(set<vector<int> > s, int n, int N)
 			b1.at(n) = 1;
 			vector<int> b2(N, 0);
 			b2.at(n) = 2;
-			snew.insert(b0);
-			snew.insert(b1);
-			snew.insert(b2);
+			snew.push_back(make_pair(b0, 0));
+			snew.push_back(make_pair(b1, 0));
+			if(k > 0) {
+				snew.push_back(make_pair(b2, 1));
+			}
 		} else {
 			vector<int> b(N, 0);
-			for(auto seq: s) {
-				for(int i = 0; i < 3; i++) {
+			for(auto tup: s) {
+				auto seq = tup.first;
+				auto k_count = tup.second;
+				int max = 3;
+				if(k_count == k) max = 2;
+				for(int i = 0; i < max; i++) {
 					b = seq;
-					b.at(n) = i;
-					snew.insert(b);
+					if(!(b.at(n) == i)) {
+						b.at(n) = i;
+						if(i == 2) k_count++;
+						snew.push_back(make_pair(b, k_count));
+					}
 				}
 			}
 		}
-		return enumerate_all_possibilities(snew, n-1, N);
+		return enumerate_all_possibilities(snew, n-1, N, k);
 	}
 }
 
 void testcase() {
+
 
 	int n, k;
 	cin >> n >> k;
@@ -48,28 +59,20 @@ void testcase() {
 
 	vector<pair<long, int> > possible_sums1;
 
-	for(auto b : enumerate_all_possibilities({}, n1-1, n1)) {
+	for(auto tup : enumerate_all_possibilities({}, n1-1, n1, k)) {
+		auto b = tup.first;
+		auto k_count = tup.second;
 		long sum_a = 0;
 		long sum_b = 0;
-		int k_count = 0;
-		bool valid = true;
 		for(int i = 0; i < n1; i++) {
 			int el = b.at(i);
 			if(el == 0) {
 				sum_a += skills.at(i);
 			} else if(el == 1) {
 				sum_b += skills.at(i);
-			} else {
-				k_count++;
-				if(k_count > k) {
-					valid = false;
-					break;
-				}
 			}
 		}
-		if(valid) {
-			possible_sums1.push_back(make_pair(sum_a - sum_b, k_count));
-		}
+		possible_sums1.push_back(make_pair(sum_a - sum_b, k_count));
 	}
 
 	sort(possible_sums1.begin(), possible_sums1.end());		// sort ascending
@@ -81,36 +84,36 @@ void testcase() {
 	}
 
 	size_t num_possibilities = 0;
-	for(auto b : enumerate_all_possibilities({}, n2-1, n2)) {
+	for(auto tup : enumerate_all_possibilities({}, n2-1, n2, k)) {
+		auto b = tup.first;
+		auto k_count = tup.second;
 		long sum_a = 0;
 		long sum_b = 0;
-		int k_count = 0;
-		bool valid = true;
 		for(int i = 0; i < n2; i++) {
 			int el = b.at(i);
 			if(el == 0) {
 				sum_a += skills.at(n1+i);
 			} else if(el == 1) {
 				sum_b += skills.at(n1+i);
-			} else {
-				k_count++;
-				if(k_count > k) {
-					valid = false;
-					break;
+			}
+		}
+		long q = -(sum_a - sum_b);
+		auto start_it = lower_bound(sums1.begin(), sums1.end(), q);
+		if(start_it != sums1.end() && *start_it == q) {
+			auto end_it = upper_bound(start_it, sums1.end(), q);
+			size_t i_start = start_it - sums1.begin();
+			size_t i_end = end_it - sums1.begin();
+			if(k1.at(i_start) + k_count <= k) {
+				int tmp_num_possibilities = 1;
+				
+				if(i_end - i_start > 1) {
+					auto it2 = upper_bound(k1.begin()+i_start, k1.begin()+i_end, k-k_count);
+					size_t i_end_real = it2 - k1.begin();
+					tmp_num_possibilities = i_end_real - i_start;
 				}
+				num_possibilities += tmp_num_possibilities;
 			}
 		}
-		if(valid) {
-			long q = -(sum_a - sum_b);
-			auto it = lower_bound(sums1.begin(), sums1.end(), q);
-
-			while(it != sums1.end() && *it == q && k1.at(it-sums1.begin()) + k_count <= k) {
-				num_possibilities++;
-				it++;
-			}
-
-		}
-
 	}
 	cout << num_possibilities << endl;
 
