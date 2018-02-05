d53ba450e153308508185eb74f17f83f208f6f1f
Philip Junker <philipjunker@gmail.com>
2018-02-05 09:26:04 +0100

    setup defensiveline

---
+++ week11/defensive_line/main.cpp
@@ -0,0 +1,20 @@
+// Includes
+// ========
+// STL includes
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void testcase() {
+
+}
+
+// Main function to loop over the testcases
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	int T;	cin >> T;
+	for (; T > 0; --T)	testcase();
+	return 0;
+}
