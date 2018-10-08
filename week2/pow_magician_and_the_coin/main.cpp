0a4a32e28119d9bbf9bccf500208f19133892d27
Philip <philipjunker@gmail.com>
2018-10-08 14:37:24 +0200

    solved pow2

--- week2/pow_magician_and_the_coin/main.cpp
+++ week2/pow_magician_and_the_coin/main.cpp
@@ -6,44 +6,33 @@
 int num_days, wealth_init, wealth_goal;
 double probability;
 
-double prob_rec(int day, int current_wealth) {
-    if(day == 0 && current_wealth > wealth_init) {
-        return 0.0;
-    } else if(current_wealth <= wealth_init) {
-        return probability;
-    } else {
-        double max_prob = prob_rec(day-1, current_wealth);
-        for(int i = current_wealth/2; i < current_wealth; i++) {
-            max_prob += prob_rec(day-1, i)*probability;
-        }
-        return max_prob;
-    }
-}
-
 void testcase() {
     std::cin >> num_days >> wealth_init >> wealth_goal;
-    std::vector<double> likelihoods(num_days);
-    for(int i = 0; i < num_days; i++) {
+    std::vector<double> likelihoods(num_days+1);
+    for(int i = 1; i <= num_days; i++) {
         std::cin >> likelihoods.at(i);
         //std::cout << likelihoods.at(i) << ' ';
     }
-    int reachable_wealth = wealth_init;
-    double probability_result;
-    int i = 0;
-    while(reachable_wealth < wealth_goal && i < num_days) {
-        reachable_wealth += reachable_wealth;
-        i++;
+    std::vector<std::vector<double> > T(wealth_goal + 1, std::vector<double>(num_days + 1, 0.0));
+    // init
+    for(int d = 0; d <= num_days; d++) {
+        T.at(wealth_goal).at(d) = 1.0;
     }
-    if(reachable_wealth < wealth_goal || i == num_days) {
-        probability_result = 0.0;   // not possible to reach wealth_goal
-    } else if(i == 0) {
-        probability_result = 1.0;   // goal is already reached before magician comes
-    } else {
-        // wealth_goal is reachable, calculate probability
-        probability = likelihoods.at(0);    // for testcases 1 and 2 we know there exists only one prob.#pragma endregion
-        probability_result = prob_rec(num_days, wealth_goal + 1);
+    for(int w = wealth_goal - 1; w*2 >= wealth_goal; w--) {
+        T.at(w).at(num_days) = likelihoods.at(num_days);
     }
-    std::cout << probability_result << std::endl;
+    for(int d = num_days-1; d > 0; d--) {
+        for(int w = wealth_goal-1; w > 0; w--) {
+            double tmp_max_prob = T.at(w).at(d+1);
+            for(int bet = 1; bet <= w; bet++) {
+                int bet_win = std::min(wealth_goal, w+bet);
+                tmp_max_prob = std::max(tmp_max_prob, likelihoods.at(d)*T.at(bet_win).at(d+1) + (1-likelihoods.at(d))*T.at(w-bet).at(d+1));
+            }
+            T.at(w).at(d) = tmp_max_prob;
+        }
+    }
+    std::cout << T.at(wealth_init).at(1) << std::endl;
+    
 }
 
 int main() {
