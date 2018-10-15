95d480365d583c3c65c7de31e2532d4df502f29b
Philip <philipjunker@gmail.com>
2018-10-15 17:14:07 +0200

    add week4

---
+++ week4/pow_hiking_maps/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+void testcase() {
+    
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false); // Always!
+    int t; std::cin >> t;
+    for(int i = 0; i < t; i++) {
+        testcase();
+    }
+    return 0;
+}
